f6a53c661d5563e67edb402ff1b6ddb6920beb2e
meronabraham <Meronabraham346@hotmail.com>
2023-12-01 18:17:54 +0100

    Calculate the median and mean

--- Program.cs
+++ Program.cs
@@ -16,6 +16,36 @@ namespace CrabAlignment
 
             //Converting input to an array of integers
             int[] positions = input.Split(',').Select(int.Parse).ToArray();
+
+            // Start with calculating the madian first
+            /*My solution will be based on the median*/
+            Array.Sort(positions);//Sorting the "positions" array in ascending order
+
+            // A new variable needed for storing the result of calculations
+            double median;
+
+            //Claculating the median for both even and odd for length of array
+            if (positions.Length % 2 == 0)
+            {
+                median = (positions[positions.Length / 2 - 1] + positions[positions.Length / 2]) / 2.0;
+            }
+            else
+            {
+                median = positions[positions.Length / 2];
+            }
+
+            // Calculate mean (average)
+            double mean = positions.Average();
+
+            // Output the calculated median and mean
+            Console.WriteLine($"Median: {median}");
+            Console.WriteLine($"Mean: {mean}");
+
+            Console.ReadLine();
+
+
+            // Based on the calulations of the median and mean calculate the fuel spent
+            /*Add the calcultions on the next commit*/
         }
     }
 }
